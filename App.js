3fc60bb53287afe117d8877d076d320c936165de
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2024-03-25 09:02:50 -0300

    Utilização do Mapview

--- App.js
+++ App.js
@@ -1,22 +1,36 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
 
 export default function App() {
+  const regiaoInicialMapa = {
+    latitude: -10,
+    longitude: -55,
+
+    /* Definição do zoom do mapa.
+    Quanto menor, mais próximo o mapa.
+    Quanto maior, mais longe o mapa fica. */
+    latitudeDelta: 40,
+    longitudeDelta: 40,
+  };
   return (
     <>
       <StatusBar />
       <View style={styles.container}>
-        <MapView />
+        <MapView
+          mapType="hybrid"
+          style={styles.mapa}
+          initialRegion={regiaoInicialMapa}
+          userInterfaceStyle="dark" // somente iOS
+          maxZoomLevel={15}
+          minZoomLevel={5}
+        />
       </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+  container: { flex: 1 },
+  mapa: { width: "100%", height: "100%" },
 });
